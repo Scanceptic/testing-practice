1342eccfbc4793e0e4c924066419693495e2a417
Nathan <urwin.nathan@gmail.com>
2024-07-15 12:10:01 +0100

    create reverseString func and test pass

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 function capitalize(string) {
 	return string[0].toUpperCase() + string.slice(1);
 }
-module.exports = capitalize;
+function reverseString(string) {
+	let reversedString = "";
+	for (let i = string.length - 1; i >= 0; i--) {
+		reversedString = reversedString.concat(string[i]);
+	}
+	return reversedString;
+}
+
+module.exports = { capitalize, reverseString };
--- main.test.js
+++ main.test.js
@@ -1,5 +1,9 @@
-const capitalize = require("./main.js");
+const { capitalize, reverseString } = require("./main.js");
 
 test("Takes in string avocadoes and makes first character capitalized", () => {
 	expect(capitalize("avocadoes")).toBe("Avocadoes");
+});
+
+test("Takes in string slapdash and returns reversed string hsadpals", () => {
+	expect(reverseString("slapdash")).toBe("hsadpals");
 });
