a31292e0b28319c2c2c6c23da93c4e244a0f438d
Nathan <urwin.nathan@gmail.com>
2024-07-15 14:02:30 +0100

    create calculator object with tests

--- main.js
+++ main.js
@@ -9,4 +9,19 @@ function reverseString(string) {
 	return reversedString;
 }
 
-module.exports = { capitalize, reverseString };
+const calculator = {
+	add: function (num1, num2) {
+		return num1 + num2;
+	},
+	subtract: function (num1, num2) {
+		return num1 - num2;
+	},
+	divide: function (num1, num2) {
+		return num1 / num2;
+	},
+	multiply: function (num1, num2) {
+		return num1 * num2;
+	},
+};
+
+module.exports = { capitalize, reverseString, calculator };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString } = require("./main.js");
+const { capitalize, reverseString, calculator } = require("./main.js");
 
 test("Takes in string avocadoes and makes first character capitalized", () => {
 	expect(capitalize("avocadoes")).toBe("Avocadoes");
@@ -6,4 +6,20 @@ test("Takes in string avocadoes and makes first character capitalized", () => {
 
 test("Takes in string slapdash and returns reversed string hsadpals", () => {
 	expect(reverseString("slapdash")).toBe("hsadpals");
+});
+
+test("Takes in calculator object and adds 1+2 to return 3", () => {
+	expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("Takes in calculator object and subtracts 4 from 11 to return 7", () => {
+	expect(calculator.subtract(11, 4)).toBe(7);
+});
+
+test("Takes in calculator object and divides 40 by 4 to return 10", () => {
+	expect(calculator.divide(40, 4)).toBe(10);
+});
+
+test("Takes in calculator object and multiplies 5 by 3 to return 15", () => {
+	expect(calculator.multiply(5, 3)).toBe(15);
 });
